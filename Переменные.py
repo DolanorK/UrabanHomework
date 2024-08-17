a83d999144fa951268f1fa9250e99274ead2efdc
DolanorK <StayAliveMarisa@gmail.com>
2024-08-18 01:58:04 +0400

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,5 @@
+Homework = 12
+Hours = 1.5
+Kurs = "Python"
+TimePerTusk = Hours/Homework
+print("Курс:",Kurs,",всего задач",Homework,",затрачено часов", Hours,",среднее время выполнения",TimePerTusk)
